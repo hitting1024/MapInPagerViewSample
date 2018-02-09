7a9d45c81b187662759d9eba1a3868554861c498
IOKA <hit0apps@gmail.com>
2018-02-10 00:53:41 +0900

    skelton activity

---
+++ app/src/main/java/jp/hitting/mapinviewpagersample/MapFragmentPagerActivity.kt
@@ -0,0 +1,12 @@
+package jp.hitting.mapinviewpagersample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MapFragmentPagerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map_fragment_pager)
+    }
+}
