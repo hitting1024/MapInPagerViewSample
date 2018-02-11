8e2210aa050b955e497e51db0a94fbd67e958686
IOKA <hit0apps@gmail.com>
2018-02-11 21:54:19 +0900

    transition to new activity

--- app/src/main/java/jp/hitting/mapinviewpagersample/MainActivity.kt
+++ app/src/main/java/jp/hitting/mapinviewpagersample/MainActivity.kt
@@ -1,12 +1,22 @@
 package jp.hitting.mapinviewpagersample
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        this.setContentView(R.layout.activity_main)
+        this.addAction()
     }
+
+    private fun addAction() {
+        this.mapFragmentButton.setOnClickListener {
+            this.startActivity(Intent(this, MapFragmentPagerActivity::class.java))
+        }
+    }
+
 }
