130e832c4fa20a98aa17adf91941d84202a5b1ff
IOKA <hit0apps@gmail.com>
2018-02-15 21:46:43 +0900

    skelton activity

---
+++ app/src/main/java/jp/hitting/mapinviewpagersample/ListViewPagerActivity.kt
@@ -0,0 +1,12 @@
+package jp.hitting.mapinviewpagersample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ListViewPagerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_view_pager)
+    }
+}
