64c8b07564da46940502f387cdb17ab17caa57fd
IOKA <hit0apps@gmail.com>
2018-02-16 22:04:41 +0900

    transition to new activity

--- app/src/main/java/jp/hitting/mapinviewpagersample/MainActivity.kt
+++ app/src/main/java/jp/hitting/mapinviewpagersample/MainActivity.kt
@@ -1,8 +1,8 @@
 package jp.hitting.mapinviewpagersample
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +16,9 @@ class MainActivity : AppCompatActivity() {
     private fun addAction() {
         this.mapFragmentButton.setOnClickListener {
             this.startActivity(Intent(this, MapFragmentPagerActivity::class.java))
+        }
+        this.listViewButton.setOnClickListener {
+            this.startActivity(Intent(this, ListViewPagerActivity::class.java))
         }
     }
 
