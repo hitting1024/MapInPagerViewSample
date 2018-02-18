9bef037a7814a97f49a018de037dd063585a6b54
IOKA <hit0apps@gmail.com>
2018-02-18 18:24:04 +0900

    add map in each listview

--- app/src/main/java/jp/hitting/mapinviewpagersample/ListViewPagerActivity.kt
+++ app/src/main/java/jp/hitting/mapinviewpagersample/ListViewPagerActivity.kt
@@ -1,12 +1,34 @@
 package jp.hitting.mapinviewpagersample
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentStatePagerAdapter
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_list_view_pager.*
 
 class ListViewPagerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_list_view_pager)
+        this.setContentView(R.layout.activity_list_view_pager)
+        val viewPager = this.viewPager
+        viewPager.adapter = ListViewPagerAdapter(this.supportFragmentManager)
     }
+}
+
+private class ListViewPagerAdapter(fragmentManager: FragmentManager) : FragmentStatePagerAdapter(fragmentManager) {
+
+    override fun getItem(position: Int): Fragment {
+        return MapListViewFragment.newInstance()
+    }
+
+    override fun getCount(): Int {
+        return 3
+    }
+
+    override fun getPageTitle(position: Int): CharSequence {
+        return "Page $position"
+    }
+
 }
