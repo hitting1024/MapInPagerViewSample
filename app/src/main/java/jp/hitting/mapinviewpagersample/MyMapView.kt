e19585dc611dbcb48b5d3969b3a38fc017348617
IOKA <hit0apps@gmail.com>
2018-02-19 00:06:39 +0900

    refactoring

--- app/src/main/java/jp/hitting/mapinviewpagersample/MyMapView.kt
+++ app/src/main/java/jp/hitting/mapinviewpagersample/MyMapView.kt
@@ -9,20 +9,13 @@ import kotlinx.android.synthetic.main.view_map.view.*
 
 class MyMapView(context: Context, fragmentManager: FragmentManager) : FrameLayout(context) {
 
-    companion object {
-        val generatedFrameLayoutId = 1234567
-    }
-
     init {
         val inflater = LayoutInflater.from(context)
         val layoutView = inflater.inflate(R.layout.view_map, null)
         val view = layoutView.mapFrameLayout
-        val frame = FrameLayout(context)
-        frame.id = generatedFrameLayoutId
-        view.addView(frame)
 
         val mapFragment = SupportMapFragment.newInstance()
-        fragmentManager.beginTransaction().add(frame.id, mapFragment).commit()
+        fragmentManager.beginTransaction().add(view.id, mapFragment).commit()
         mapFragment.getMapAsync {
             // nothing
         }
