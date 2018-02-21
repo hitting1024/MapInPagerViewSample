e395f1e74e43f749dd6557a7ff9cdb246918999d
IOKA <hit0apps@gmail.com>
2018-02-21 23:50:11 +0900

    add map in each fragment

--- app/src/main/java/jp/hitting/mapinviewpagersample/FragmentPagerActivity.kt
+++ app/src/main/java/jp/hitting/mapinviewpagersample/FragmentPagerActivity.kt
@@ -1,13 +1,35 @@
 package jp.hitting.mapinviewpagersample
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentStatePagerAdapter
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_fragment_pager.*
 
 class FragmentPagerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_fragment_pager)
+        this.setContentView(R.layout.activity_fragment_pager)
+        val viewPager = this.viewPager
+        viewPager.adapter = MapPagerFragmentAdapter(this.supportFragmentManager)
+    }
+
+}
+
+private class MapPagerFragmentAdapter(fragmentManager: FragmentManager) : FragmentStatePagerAdapter(fragmentManager) {
+
+    override fun getItem(position: Int): Fragment {
+        return MapPagerFragment.newInstance()
+    }
+
+    override fun getCount(): Int {
+        return 3
+    }
+
+    override fun getPageTitle(position: Int): CharSequence {
+        return "Page $position"
     }
 
 }
